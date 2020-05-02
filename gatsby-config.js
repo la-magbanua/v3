fe8821f9b81bdf440a16a2332263c4609d7ed64c
la-magbanua <la.magbanua18@gmail.com>
2020-05-03 07:51:23 +0800

    changed analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || none,
+        trackingId: process.env.ANALYTICS_TRACKING_ID || none,
       },
     },
     `gatsby-plugin-react-helmet`,
